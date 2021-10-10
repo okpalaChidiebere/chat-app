a40640ad81e629d7d90900565cc38b9c7d751bc1
okpalaChidiebere <okpalacollins4@gmail.com>
2021-10-09 19:13:11 -0600

    Make the Gifted Chat UI work with our ws backend
    - We made sure the message sent back from the ws server is in the json format that GiftedChat understands
    - We implemeted best server security practise with ws where the user is not allowed to send use ids over the ws. They can only send their own ws connection id if we choose them to at most. This helps for a user not being able to pretend as others to send a message over the ws.

--- MobileChatApp/App.js
+++ MobileChatApp/App.js
@@ -1,61 +1,76 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet } from "react-native";
 import { GiftedChat } from "react-native-gifted-chat"; //https://github.com/FaridSafi/react-native-gifted-chat
 
 const socketUrl = "127.0.0.1:8080";
 
 export default function App() {
-  const [messageToSend, setMessageToSend] = React.useState("");
   const [recvMessages, setRecvMessages] = React.useState([]);
   const socket = React.useRef(null);
 
   React.useEffect(() => {
     socket.current = new WebSocket(`ws://${socketUrl}`);
 
-    setRecvMessages([
-      {
-        _id: 1,
-        text: "Hello developer",
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
-        },
-      },
-    ]);
-
     //listen for new messages send from our ws server to us
     socket.current.onmessage = (message) => {
-      setRecvMessages((currState) => [...currState, message.data]);
+      const msg = JSON.parse(message.data);
+
+      /**
+       * Our server is responsible for creating the message object correctly,
+       * then the UI will now have to render the message on the left or right side(the one we send)
+       * based on user._id value
+       */
+      setRecvMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, msg)
+      );
     };
   }, []);
 
-  const sendMessage = () => {
-    socket.current.send(messageToSend); //send a message through the webSocket connection
-    setMessageToSend(""); //clear the TextInput
-  };
+  const sendMessage = React.useCallback((messages = []) => {
+    /**
+     * The reason we are only sending the text string value instead of the full object message data is
+     * because we dont want to trust the client to send their userId themselves. The
+     * user can easily switch the user._id and it will look like other persons sent the message
+     *
+     * Once the user connects to our ws server, the backend will assign then a
+     */
+    if (socket.current.readyState === socket.current.OPEN)
+      socket.current.send(messages[0].text); //we need just the text value to send to our ws backend which will broadcast this message to others
+
+    /**
+     * We update our UI optimistically; if the message dont get broadcasted due to
+     * our ws connection is closed, we could always catch this error and rollback this
+     * UI update and let the user know that there is a network issue on this end.
+     *
+     * By default the messages[0].user._id will be 1 as specified in the GiftedChat user props.
+     * This message will appear on the right
+     */
+    setRecvMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
 
   return (
     <>
       <StatusBar style="auto" />
       <GiftedChat
         messages={recvMessages}
-        /*onSend={(messages) => onSend(messages)}*/
+        onSend={(messages) => sendMessage(messages)}
         user={{
+          /**
+           * Gifted messages with userId 1 appears on the right side of the chat UI with a default
+           * blue color background container view
+           *
+           * When the userId is 1, it means that we wrote the message. If we change the id to 2,
+           * then all messages with user._id of 2 will appear on the right as every other messages
+           * will appear on the left
+           *
+           * NOTE: in real App, we would use uuid as userIds and thesame concepts will apply as well.
+           * But for this demo, this will do
+           */
           _id: 1,
         }}
       />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- web-server/server.js
+++ web-server/server.js
@@ -8,16 +8,48 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+let currentUserId = 2; // we started at 2 because we dont want the chat messages sent out to appear on the right side of the GiftedChat UI. NOTE: we are using integers here because our GiftedChat uses numbers as userIds. In real App we would use uuid
+const userIds = {}; //Create a directory of userIds with each id mapped to a ws id connection. Learn more on managing ids here https://stackoverflow.com/questions/13364243/websocketserver-node-js-how-to-differentiate-clients
+
+function createMessage(userId, messageText) {
+  return {
+    _id: Math.random().toString(36).substr(-8), //in real app, we will use uuid
+    text: messageText,
+    createdAt: Date.now(),
+    user: {
+      _id: userId,
+      name: "React Native",
+      avatar: "https://placeimg.com/140/140/any",
+    },
+  };
+}
+
 wss.on("connection", (ws) => {
-  console.log("A user connected!");
+  //NOTE: this id property was added by us ourself. the ws value dont have this by default
+  ws.id = Math.random().toString(36).substr(-8); // as assign a uniqueID to this user. Ideally we will use uuid but this will do for dev :)
+  userIds[ws.id] = currentUserId++; //we map each unique ws id connection to the userIds
+
+  console.log(`A user connected! ${ws.id}`);
+
+  ws.on("close", () => {
+    /** FYI: For Android and iOS device, ws connections only closes when the app is terminated, internal server-side error or the device losses wifi or data connections while app is in background or active */
+    console.log(`User disconnected: ${ws.id}`);
+    // remove from the map
+    delete userIds[ws.id];
+  });
   //We listen for websocket connections here by adding WebSocket EventListeners
   ws.on("message", (message, isBinary) => {
-    console.log(message.toString()); //FYI: we used the toString() to convert the message from binary to human readable data
+    const messageText = message.toString(); //FYI: we used the toString() to convert the message from binary to human readable data
 
     //broadcast this new message received to all clients currently connected to our ws server
     wss.clients.forEach((client) => {
+      if (ws === client) return; //we broadcast this message to everyone except the sender
+
       if (client.readyState === WebSocket.OPEN) {
-        client.send(message.toString());
+        const userId = userIds[client.id]; //we get the userId from our server! we dont have to rely on the client to supply this userId which is not a good practice
+        const broadCastMessage = createMessage(userId, messageText);
+        //console.log(broadCastMessage);
+        client.send(JSON.stringify(broadCastMessage));
       }
     });
   });
